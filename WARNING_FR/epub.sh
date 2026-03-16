7dcaf64c19de71f46da3913e8c033a715829fe68
Sylvain Saucier <62949224+sysaulab@users.noreply.github.com>
2026-03-15 17:21:21 -0700

    Build cleanup
    Added proper page numbers and table of content in the PDF and added a release code variable in the build script to mark files appropriately. We are in FE (First Edition)

--- WARNING_FR/epub.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-pandoc CHAPTERS/*.md PAPERS/*.md --toc --epub-cover-image=COVER/FRONT.jpg -o output.epub
-
--- build_documents.sh
+++ build_documents.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-pandoc WARNING_EN/CHAPTERS/*.md WARNING_EN/PAPERS/*.md -s --toc --epub-cover-image=WARNING_EN/COVER/FRONT.jpg -o WarNing_english.epub
-pandoc WARNING_EN/CHAPTERS/*.md WARNING_EN/PAPERS/*.md -s -o WarNing_english.html
-pandoc WARNING_EN/CHAPTERS/*.md WARNING_EN/PAPERS/*.md -s -o WarNing_english.md
-pandoc WARNING_EN/CHAPTERS/*.md WARNING_EN/PAPERS/*.md -s -o WarNing_english.pdf --pdf-engine=weasyprint --css style_pdf_6x9.css
+# FE First Edition
 
-pandoc WARNING_FR/CHAPTERS/*.md WARNING_FR/PAPERS/*.md -s --toc --epub-cover-image=WARNING_FR/COVER/FRONT.jpg -o WarNing_francais.epub
-pandoc WARNING_FR/CHAPTERS/*.md WARNING_FR/PAPERS/*.md -s -o WarNing_francais.html
-pandoc WARNING_FR/CHAPTERS/*.md WARNING_FR/PAPERS/*.md -s -o WarNing_francais.md
-pandoc WARNING_FR/CHAPTERS/*.md WARNING_FR/PAPERS/*.md -s -o WarNing_francais.pdf --pdf-engine=weasyprint --css style_pdf_6x9.css
+edition="FE"
+
+pandoc WARNING_EN/CHAPTERS/*.md WARNING_EN/PAPERS/*.md -s --toc --toc-depth=1 --epub-cover-image=WARNING_EN/COVER/FRONT.jpg -o "WarNing_($edition)_english.epub" 
+pandoc WARNING_EN/CHAPTERS/*.md WARNING_EN/PAPERS/*.md -s -o "WarNing_($edition)_english.html"
+pandoc WARNING_EN/CHAPTERS/*.md WARNING_EN/PAPERS/*.md -s -o "WarNing_($edition)_english.txt"
+pandoc WARNING_EN/CHAPTERS/*.md WARNING_EN/PAPERS/*.md -s -o "WarNing_($edition)_english(booklet).pdf" --pdf-engine=weasyprint --toc --toc-depth=1 --css style_pdf_booklet.css
+pandoc WARNING_EN/CHAPTERS/*.md WARNING_EN/PAPERS/*.md -s -o "WarNing_($edition)_english(letter).pdf" --pdf-engine=weasyprint --toc --toc-depth=1 --css style_pdf_letter.css
+
+pandoc WARNING_FR/CHAPTERS/*.md WARNING_FR/PAPERS/*.md -s --toc --toc-depth=1 --epub-cover-image=WARNING_FR/COVER/FRONT.jpg -o "WarNing_($edition)_francais.epub"
+pandoc WARNING_FR/CHAPTERS/*.md WARNING_FR/PAPERS/*.md -s -o "WarNing_($edition)_francais.html"
+pandoc WARNING_FR/CHAPTERS/*.md WARNING_FR/PAPERS/*.md -s -o "WarNing_($edition)_francais.txt"
+pandoc WARNING_FR/CHAPTERS/*.md WARNING_FR/PAPERS/*.md -s -o "WarNing_($edition)_francais(booklet).pdf" --pdf-engine=weasyprint --toc --toc-depth=1 --css style_pdf_booklet.css
+pandoc WARNING_FR/CHAPTERS/*.md WARNING_FR/PAPERS/*.md -s -o "WarNing_($edition)_francais(lettre).pdf" --pdf-engine=weasyprint --toc --toc-depth=1 --css style_pdf_letter.css
 
